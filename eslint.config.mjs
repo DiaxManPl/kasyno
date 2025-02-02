93a338599bcfd620d3c5ecc433dfd8fbe352a980
DiaxManPl <diaxman.gameplay@gmail.com>
2025-02-02 13:33:43 +0100

    chore(eslint): refactor config
    Idk why I'm putting so much effort into configuring eslint for a school project, but maybe I will use this as a boilerplate for some real projects.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
 import tailwind from "eslint-plugin-tailwindcss";
 import prettier from "eslint-config-prettier";
 
@@ -12,16 +15,38 @@ const compat = new FlatCompat({
 	baseDirectory: __dirname,
 });
 
-/** @type {import('eslint').Linter.Config[]} */
-// eslint-disable-next-line import/no-anonymous-default-export
-export default [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
+export default tseslint.config(
+	{
+		ignores: [".next"],
+	},
+
+	// Base eslint
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+
+	// Next.js & React
+	...compat.extends("plugin:@next/next/recommended"),
+	...compat.extends("plugin:react/recommended"),
+	...compat.extends("plugin:react-hooks/recommended"),
+
+	// Other plugins
 	...tailwind.configs["flat/recommended"],
 	prettier,
 
+	// Settings
 	{
+		settings: {
+			react: {
+				version: "detect",
+			},
+			tailwindcss: {
+				callees: ["clsx", "cn"],
+			},
+		},
+
 		rules: {
-			"tailwindcss/classnames-order": "off",
-		}
+			"react/react-in-jsx-scope": "off", // It's not 2017 anymore, why is this still on by default
+			"tailwindcss/classnames-order": "off", // This is already handled by Prettier
+		},
 	},
-];
+);
